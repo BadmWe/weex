c3d77922dd8bebb352ed684a853ca553669bf4c8
pfed-prog <fedorovn19@gmail.com>
2025-12-29 21:04:24 +0100

    openai agent runs

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
 # AI Agent Bot
 
+## To run
+
+`python -m venv venv`
+
+`source ./venv/Scripts/activate`
+
+`pip install -r requirements.txt`
+
 ## ENV File
 
-`GOOGLE_API_KEY="...data"`
+`OPENAI_API_KEY="...data"`
 
 ## Further Resources
 
-https://medium.com/@dvasquez.422/building-a-simple-ai-agent-1e2f2b369b25
+### Weex
+
+[Get Single Ticker](https://www.weex.com/api-doc/contract/Market_API/GetTickerInfo)
+
+[Participation Guide](https://www.weex.com/api-doc/ai/introduction/ParticipantGuide)
+
+### LangChain
+
+[Building a Simple AI Agent With Python and Langchain](https://medium.com/@dvasquez.422/building-a-simple-ai-agent-1e2f2b369b25)
 
 https://github.com/NoManNayeem/Langchain_CrewAI_Gemini-AI_Agents/tree/main
 
 https://github.com/google-gemini/cookbook
 
 https://github.com/MikeChan-HK/Gemini-agent-example
-
-https://www.weex.com/api-doc/ai/introduction/ParticipantGuide
---
+++ gpt.py
@@ -0,0 +1,70 @@
+import requests
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langchain.messages import SystemMessage, HumanMessage
+
+model = ChatOpenAI(model="gpt-5-nano")
+
+@tool
+def get_mexc_futures_ADA():
+    """Get Futures MEXC Data for ADA and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/ADA_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_BNB():
+    """Get Futures MEXC Data for BNB and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/BNB_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_BTC():
+    """Get Futures MEXC Data for BTC and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/BTC_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_DOGE():
+    """Get Futures MEXC Data for DOGE and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/DOGE_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_ETH():
+    """Get Futures MEXC Data for ETH and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/ETH_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_LTC():
+    """Get Futures MEXC Data for LTC and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/LTC_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_SOL():
+    """Get Futures MEXC Data for SOL and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/SOL_USDT').json()
+    return r
+
+@tool
+def get_mexc_futures_XRP():
+    """Get Futures MEXC Data for XRP and usdt"""
+    r = requests.get(f'https://contract.mexc.com/api/v1/contract/depth/XRP_USDT').json()
+    return r
+
+
+@tool
+def get_mexc_ticker():
+    """Get Futures MEXC Data for 832 trading pairs includes data for last price and 24 hour window detailed data"""
+    r = requests.get('https://contract.mexc.com/api/v1/contract/ticker').json()
+    return r
+
+agent = create_agent(model, tools=[get_mexc_futures, get_mexc_futures_ADA, get_mexc_futures_BTC, get_mexc_futures_DOGE, get_mexc_futures_BNB, get_mexc_futures_ETH, get_mexc_futures_LTC, get_mexc_futures_SOL, get_mexc_futures_XRP], system_prompt="You are a helpful Senior Research Analyst. Analyze the major trading pairs involving ADA, SOL, LTC, DOGE, BTC, ETH, XRP, BNB on CEX Weex. Be concise and accurate.")
+
+result = agent.invoke(
+    {"messages": [HumanMessage("Conduct a comprehensive analysis of the tickers ibcluding spot and futures. Identify key trends and document your analysis. Suggest trading actions: stop limits, time window and a ticker to trade.")]}
+)
+
+print(result)
